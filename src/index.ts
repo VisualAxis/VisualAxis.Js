b4086e6d557dee0205520429f880a80033fc5ef0
Leonas Carstens <leonascarstens@MBP-von-Leonas.fritz.box>
2026-06-28 14:16:29 +0200

    added basic render function and test html file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world!')
+class Chart {
+    private element: HTMLElement;
+    constructor(element: HTMLElement) {
+        this.element = element;
+    }
+
+    public render() {
+        this.element.innerHTML = `<h1>Hello World!</h1>`;
+    }
+}
+
+// Expose the Chart class globally
+if (typeof globalThis !== 'undefined') {
+    (globalThis as any).Chart = Chart;
+}
